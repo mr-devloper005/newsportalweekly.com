cac0700d18b3aca8d265d631a00fb1df1d656b5f
root <root@srv1489383.hstgr.cloud>
2026-05-21 13:56:02 +0000

    Prevent cached 404 on detail pages

--- src/app/pdf/[slug]/page.tsx
+++ src/app/pdf/[slug]/page.tsx
@@ -13,6 +13,9 @@ import { buildPostUrl, fetchTaskPostBySlug, fetchTaskPosts } from "@/lib/task-da
 import { SITE_CONFIG } from "@/lib/site-config";
 import { ArticleShareWrapper } from "@/components/tasks/article-share-wrapper";
 
+
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 export const revalidate = 3;
 
 export async function generateStaticParams() {
